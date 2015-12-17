25cacb9e27ec3b02f90b897c4c7b520e488a7e3b
Hasan Quadri <hasan.quadri@ca.com>
2015-12-17 18:31:30 -0500

    Execute container test and display output to console before prompting user for input

--- demo/repo/SyndicateFlow.groovy
+++ demo/repo/SyndicateFlow.groovy
@@ -28,6 +28,18 @@ node
       appcontainer = appimage.run('-d -i -p 8082:8080 --name demoapp_integration$BUILD_ID')
       testcontainer = testimage.run('-d -i -p 8083:8080 --link demoapp_integration$BUILD_ID:app --name demotest_integration$BUILD_ID')
 
+      echo '***** Wait for app and test containers to start up'
+      retry(10)
+      {
+         sleep 3
+         sh 'docker exec -t demoapp_integration$BUILD_ID curl --silent --show-error localhost:8080'
+         sh 'docker exec -t demotest_integration$BUILD_ID curl --silent --show-error localhost:8080'
+      }
+
+      echo '***** Execute test on test container'
+      // This will output results to console output
+      sh 'docker exec -t demotest_integration$BUILD_ID curl --write-out "\n" localhost:8080/test'
+
       input "Integration image ok?"
 
       echo '***** Image verified...Tagging images for preproduction'
@@ -58,6 +70,18 @@ node
    {
       appcontainer = appimage.run('-d -i -p 8084:8080 --name demoapp_preprod$BUILD_ID')
       testcontainer = testimage.run('-d -i -p 8085:8080 --link demoapp_preprod$BUILD_ID:app --name demotest_preprod$BUILD_ID')
+
+      echo '***** Wait for app and test containers to start up'
+      retry(10)
+      {
+         sleep 3
+         sh 'docker exec -t demoapp_preprod$BUILD_ID curl --silent --show-error localhost:8080'
+         sh 'docker exec -t demotest_preprod$BUILD_ID curl --silent --show-error localhost:8080'
+      }
+
+      echo '***** Execute test on test container'
+      // This will output results to console output
+      sh 'docker exec -t demotest_preprod$BUILD_ID curl --write-out "\n" localhost:8080/test'
 
       input "Pre-production image ok?"
 
