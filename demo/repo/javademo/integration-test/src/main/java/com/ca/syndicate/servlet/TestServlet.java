c3a714f3dcf0f969dfb46be97090ad633b30b23b
Hasan Quadri <hasan.quadri@ca.com>
2016-01-07 12:16:15 -0500

    Added directory for Jan 2016 demo using proposed Maven build structure; Added skeleton for integration test servlet.

---
+++ demo/repo/javademo/integration-test/src/main/java/com/ca/syndicate/servlet/TestServlet.java
@@ -0,0 +1,29 @@
+package com.ca.syndicate.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		response.getWriter().write("<html><body><h1>from servlet</h1></body></html>");
+	}
+}
