c1172b2f6ad3ac96e0079502e7986f58411dc549
Hasan Quadri <hasan.quadri@ca.com>
2016-01-14 17:53:28 -0500

    Start app and integration test containers and then run the integration test.

--- demo/repo/new-demo.groovy
+++ demo/repo/new-demo.groovy
@@ -33,7 +33,7 @@ node {
     
     def buildContainerId = 'syndicatebuild' + env.BUILD_ID
     
-    
+    echo '***** Start container to run build and unit tests'
     // Start the Maven build container and execute the build.
     // NOTES:
     // Use -t and --entrypoint=cat to keep the container running until withRun
@@ -51,20 +51,93 @@ node {
         {
             sh 'docker exec -t ' + buildContainerId + ' bash -c "cd /usr/build/demo/repo/javademo && mvn clean package"'
         }
+
+        echo '***** Build and unit tests were successful'
     }
     catch (all)
     {
         error 'Build or unit test failed'
     }
 
+    def appimage
+    def testimage
+    
     // Build the Docker images for the app and integration test
     parallel "Building Docker app image":
     {
-        appimage = docker.build('demoapp:1.0','demo/repo/javademo/app')
+        appimage = docker.build('demoapp:ci','demo/repo/javademo/app')
     },
     "Building Docker integration test image":
     {
-        testimage = docker.build('demotest:1.0','demo/repo/javademo/integration-test')
+        testimage = docker.build('demotest:ci','demo/repo/javademo/integration-test')
     },
     failFast: true
+    
+    echo '***** Docker builds for images successful'
+    
+    def appcontainer
+    def testcontainer
+    echo '***** Integration test stage...Start Docker containers for integration testing'
+    try
+    {
+        appcontainer = appimage.run('-d -i -p 8082:8080 --name demoapp_ci$BUILD_ID')
+        testcontainer = testimage.run('-d -i -p 8083:8080 --link demoapp_ci$BUILD_ID:app --name demotest_ci$BUILD_ID')
+
+        echo '***** Preparing for integration testing...Wait for app and test containers to start up'
+        retry(10)
+        {
+            sleep 3
+            sh 'docker exec -t demoapp_ci$BUILD_ID curl --write-out "\n" localhost:8080/demoapp/status'
+            sh 'docker exec -t demotest_ci$BUILD_ID curl --write-out "\n" localhost:8080/demotest/status'
+        }
+
+        echo '***** Execute test on integration test container'
+        // This will execute the test from inside the test container.
+        // The output will be piped to a file so that it can be read into a
+        // variable to process the test output.
+        sh 'docker exec -t demotest_ci$BUILD_ID curl --write-out "\n" localhost:8080/demotest/test > test.txt 2>&1'
+        def testoutput = readFile('test.txt')
+        echo testoutput
+
+        if (testoutput.contains('pass: true'))
+        {
+            echo '***** Integration test passed.'
+        }
+        else
+        {
+            error 'Contract test failed...Aborting build.'
+        }
+    }
+    catch (all)
+    {
+        // Force build failure.
+        // Don't go any futher after cleanup in finally block
+        error 'Integration test stage failed'
+    }
+    finally
+    {
+        parallel "Stop app container":
+        {
+            // Executes when user aborts from input.
+            // Stops and removes containers
+            try
+            {
+                appcontainer.stop()          
+            }
+            catch (all)
+            {    
+            }
+        },
+        "Stop integration test container":
+        {
+            try
+            {
+                testcontainer.stop()          
+            }
+            catch (all)
+            {
+            }
+        },
+        failFast: false
+    }
 }
