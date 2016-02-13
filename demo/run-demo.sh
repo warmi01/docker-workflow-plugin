695bdff805f838eb9e3dfe46f58fa8c74772261c
Hasan Quadri <hasan.quadri@ca.com>
2016-02-12 19:34:17 -0500

    Use $JENKINS_HOME in scripts and need to use different mount location since /var/jenkins_home is locked as a Docker volume.

--- demo/run-demo.sh
+++ demo/run-demo.sh
@@ -41,10 +41,21 @@ echo '***************         Docker Registry Service running now             **
 # In case some tagged images were left over from a previous run using a cache:
 (docker images -q examplecorp/spring-petclinic; docker images -q docker.example.com/examplecorp/spring-petclinic) | xargs docker rmi --no-prune=true --force
 
+# quaha01:
+# Appformer package sets an NFS mount point that we can't change.
+# Perform a one-time create of a soft link to the NFS mount point.
+# This will occur for each new container/POD.
+if [ "$DSS_LOCALMOUNTPOINT_JENKINS" ] && [ -e /var/firststart ]
+then
+    rm -f /var/firststart
+    ln -s -f $DSS_LOCALMOUNTPOINT_JENKINS $JENKINS_HOME
+    echo "Created soft link from $JENKINS_HOME to NFS mount point $DSS_LOCALMOUNTPOINT_JENKINS"
+fi
+
 #
 # Remove the base workflow-demo "cd" job
 #
-rm -rf /usr/share/jenkins/ref/jobs/cd /var/jenkins_home/jobs/cd
+rm -rf /usr/share/jenkins/ref/jobs/cd $JENKINS_HOME/jobs/cd
 
 #
 # Now run Jenkins.
